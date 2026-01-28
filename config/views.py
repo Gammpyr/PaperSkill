14b3dd87596a09fb09403a90c831f851476e7cf7
Gamid Abdulkhalikov <malak93@yandex.ru>
2026-01-28 20:31:38 +0300

    update config/views.py

--- config/views.py
+++ config/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 from django.views.generic import TemplateView
 
 
@@ -7,3 +8,13 @@ class IndexView(TemplateView):
 
 def home(request):
     return HttpResponse("<h1>Добро пожаловать в PaperSkill!</h1><p>Это главная страница Django-проекта.</p>")
+
+class ContactView(TemplateView):
+    template_name = "paperskill/contacts.html"
+
+    def post(self, request, *args, **kwargs):
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+
+        return render(request, 'paperskill/answer.html', {'name': name, 'email': email})
