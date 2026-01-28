1b92c06eb797f10d666cd087484d5086efe7f571
Gamid Abdulkhalikov <malak93@yandex.ru>
2026-01-28 17:52:23 +0300

    update users/views.py (add PaymentViewSet)

--- users/views.py
+++ users/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from rest_framework import generics
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import generics, viewsets
+from rest_framework.exceptions import ValidationError
+from rest_framework.filters import OrderingFilter
 
-from users.models import User
-from users.serializers import CustomUserSerializer
+from users.models import User, Payment
+from users.serializers import CustomUserSerializer, PaymentSerializer
+from users.services import create_stripe_product, create_stripe_price, create_stripe_session
 
 
 class UserCreateAPIView(generics.CreateAPIView):
@@ -18,6 +22,32 @@ class UserListAPIView(generics.ListAPIView):
     serializer_class = CustomUserSerializer
     queryset = User.objects.all()
 
+
+class PaymentViewSet(viewsets.ModelViewSet):
+    serializer_class = PaymentSerializer
+    queryset = Payment.objects.all()
+
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ["paid_course", "paid_lesson", "payment_method"]
+    ordering_fields = ["payment_date"]
+
+    def perform_create(self, serializer):
+        try:
+            payment = serializer.save(user=self.request.user)
+
+            product_name = payment.paid_course.name if payment.paid_course else payment.paid_lesson.name
+
+            product = create_stripe_product(product_name)
+            price = create_stripe_price(product, payment.payment_amount)
+            session = create_stripe_session(price)
+
+            payment.session_id = session.get("id")
+            payment.payment_url = session.get("url")
+            payment.product_id = product.get("id")
+            payment.price_id = price.get("id")
+            payment.save()
+        except Exception as e:
+            raise ValidationError(f"Ошибка при создании сессии оплаты: {str(e)}")
 
 
 
