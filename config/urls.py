5a6482f6a07ed094a2deb0a51718f7518b976c96
Gamid Abdulkhalikov <malak93@yandex.ru>
2026-01-28 20:31:43 +0300

    update config/urls.py

--- config/urls.py
+++ config/urls.py
@@ -22,8 +22,9 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', views.home, name='home'),
+
     path('', views.IndexView.as_view(), name='home'),
+    path('contacts/', views.ContactView.as_view(), name='contacts'),
 
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
