ff5670b768703005a8d69c20e85639016d15e172
Gamid Abdulkhalikov <malak93@yandex.ru>
2026-01-28 17:52:07 +0300

    update users/urls.py

--- users/urls.py
+++ users/urls.py
@@ -3,10 +3,12 @@ from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.apps import UsersConfig
-from . import views
+from users import views
 
 app_name = UsersConfig.name
 
+router = DefaultRouter()
+router.register(r"payments", views.PaymentViewSet, basename="payments")
 
 urlpatterns = [
     path('users/create/', views.UserCreateAPIView.as_view(), name='users-create'),
@@ -15,6 +17,4 @@ urlpatterns = [
 
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-
-
-]
+] + router.urls
