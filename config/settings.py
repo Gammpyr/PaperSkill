b32a76dc8a4fe2dc17dd1798aca4cfe9687e2214
Gamid Abdulkhalikov <malak93@yandex.ru>
2026-01-28 23:52:24 +0300

    update configs

--- config/settings.py
+++ config/settings.py
@@ -138,6 +138,7 @@ PHONENUMBER_DEFAULT_REGION = "RU"
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ],
 }
 
--- config/views.py
+++ config/views.py
@@ -1,10 +1,23 @@
+from django.contrib.auth import get_user_model
+from django.db.models import Count
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+from paperskill.models import Course
+from users.models import User
+
 
 class IndexView(TemplateView):
     template_name = "paperskill/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['courses_count'] = Course.objects.count()
+        context['students_count'] = User.objects.count()
+        context['courses'] = Course.objects.annotate(lesson_count=Count('lessons')).order_by('-created_at')[:3]
+
+        return context
 
 def home(request):
     return HttpResponse("<h1>Добро пожаловать в PaperSkill!</h1><p>Это главная страница Django-проекта.</p>")
