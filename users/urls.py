fae95eb5462aabfe15ce6c3f1337a757335886b7
Gamid Abdulkhalikov <malak93@yandex.ru>
2026-01-28 20:32:17 +0300

    update users/urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,11 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.apps import UsersConfig
 from users import views
+from users.forms import CustomAuthenticationForm
 
 app_name = UsersConfig.name
 
@@ -11,10 +13,16 @@ router = DefaultRouter()
 router.register(r"payments", views.PaymentViewSet, basename="payments")
 
 urlpatterns = [
-    path('users/create/', views.UserCreateAPIView.as_view(), name='users-create'),
-    path('users/', views.UserListAPIView.as_view(), name='users-list'),
-    path('users/update/<int:pk>/', views.UserUpdateAPIView.as_view(), name='users-update'),
+                  path('users/create/', views.UserCreateAPIView.as_view(), name='users-create'),
+                  path('users/', views.UserListAPIView.as_view(), name='users-list'),
+                  path('users/update/<int:pk>/', views.UserUpdateAPIView.as_view(), name='users-update'),
 
-    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-] + router.urls
+                  path('profile/', views.ProfileView.as_view(), name='profile'),
+
+                  path('register/', views.RegisterView.as_view(), name='register'),
+                  path('login/', LoginView.as_view(template_name='users/login.html', authentication_form=CustomAuthenticationForm), name='login'),
+                  path('logout/', LogoutView.as_view(next_page='paperskill:courses_list'), name='logout'),
+
+                  path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+                  path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+              ] + router.urls
